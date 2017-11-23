d18311c3d8e1f5fb3a771473bc70721417fe7931
Adam Faiers <FairZ@users.noreply.github.com>
2017-11-23 12:41:53 +0000

    Commented ray.cpp

--- src/Ray.cpp
+++ src/Ray.cpp
@@ -29,10 +29,14 @@ bool Ray::calcHitPoint(Face _face, aml::Vector &hitPoint)
 
 bool Ray::MollerTrumboreIntersection(MollerTrumboreFace _face, float &_t)
 {
+	//calculate and store P for later use in the algorithm
 	aml::Vector P = aml::CrossProduct(direction, _face.edge2);
-
+	
+	//calculate and store the determinant for use in back face culling and divide by zero error checking
 	float determinant = aml::DotProduct(_face.edge1, P);
 
+	//if the determinant is less than or equal to zero the ray is either parallel to or facing the back of
+	//the triangle face so exit the function and return no hit
 	if(determinant <= 0.0f)
 		return false;
 
@@ -40,14 +44,19 @@ bool Ray::MollerTrumboreIntersection(MollerTrumboreFace _face, float &_t)
 
 	aml::Vector T = origin - _face.vert0;
 	float u = aml::DotProduct(T, P) * inverseDeterminant;
+	//if u is less than 0 or greater than 1 the hit point is not contained within the triangle
+	//so exit the funciton and return no hit
 	if (u < 0 || u > 1)
 		return false;
 
 	aml::Vector Q = aml::CrossProduct(T, _face.edge1);
 	float v = aml::DotProduct(direction, Q) * inverseDeterminant;
+	//if v is less than 0 or u+v is greater than 1 the hit point is not contained within the triangle
+	//so exit the funciton and return no hit
 	if (v < 0 || (u + v) > 1)
 		return false;
 
+	//calculate t and check whether it is within the rendering cube otherwise discard the hit
 	_t = aml::DotProduct(_face.edge2, Q) * inverseDeterminant;
 	if (_t < 0 || _t > 256)
 		return false;
